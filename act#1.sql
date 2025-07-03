c413eaed5e5682770203984d0b677148a2d8bae7
VHIERON BAREZA <2120336@cc.usls.edu>
2025-07-03 11:17:23 +0800

    act 1 (unfinished)

---
+++ act#1.sql
@@ -0,0 +1,60 @@
+-- problem 1: online bookstore inventory
+
+create table if not exists authors (
+    author_id int(10),
+    first_name varchar(100),
+    last_name varchar(100),
+    biography text,
+    dob date
+);
+
+create table if not exists publishers (
+    publisher_id int(10),
+    publishers_name varchar(100),
+    origin_country varchar(100)
+);
+
+create table if not exists books (
+    book_id int(10),
+    title varchar(100),
+    isbn int(13),
+    publication date,
+    price decimal(10,2)
+);
+
+create table if not exists book_authors (
+    -- a table to resolve the many-to-many relationship between books and authors.
+    -- it should contain foreign keys referencing the books and authors tables.
+    -- the combination of a book and an author must be unique.
+    -- if a book or an author is deleted, the corresponding record in this table should also be deleted.
+);
+
+-- problem 2: clinic appointment system
+
+create table if not exists patients (
+    patient_id int(10),
+    first_name varchar(100),
+    last_name varchar(100),
+    contact int(20),
+    patient_record date
+);
+
+create table if not exists doctors (
+    doctor_id int(10),
+    first_name varchar(100),
+    last_name varchar(100),
+    specialty varchar(100),
+    license int(4) 
+);
+
+create table if not exists appointments (
+    appointment_id int(10),
+    -- foreign keys to link to one patient and one doctor
+    date_time datetime,
+    reason text,
+    -- the status of the appointment, which can only be one of the following: 'Scheduled', 'Completed', 'Canceled', 'No-Show'. The default should be 'Scheduled'.
+    -- if a patient or doctor is deleted from the system, their appointments should also be deleted.
+);
+
+-- problem 3: university course catalog
+
